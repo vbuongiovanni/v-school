fabe3c1a5b683bb16f69025a6a96c43607c522dd
Vince Buongiovanni <vincentbuongiovanni@gmail.com>
2022-06-02 19:10:38 -0600

    implemented click to copy CSS code to clipboard on CSS Gradient Code Generator assignment.

--- level-4/assignments/css-gradient-generator-tool/src/components/PreviewPane.js
+++ level-4/assignments/css-gradient-generator-tool/src/components/PreviewPane.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 
 export default function PreviewPane(props) {
 
     const {colorOne, colorTwo, colorThree, angle} = props.styleConfigurations;
     const {showButtonThree} = props;
 
+    // set state for copy to clipboard functionality
+    const [copiedText, setCopiedText] = useState("")
+
     // ternary to handle logic of third color
     const colorThreeStyle = showButtonThree ? `, ${colorThree}` : "";
 
     // text that will be presented on UI
     let cssPresentation = `background: linear-gradient(${angle}deg, ${colorOne}, ${colorTwo}${colorThreeStyle});<br>
-        -moz-background: linear-gradient(${angle}deg, ${colorOne} , ${colorTwo}${colorThreeStyle});<br>
+        -moz-background: linear-gradient(${angle}deg, ${colorOne}, ${colorTwo}${colorThreeStyle});<br>
         -webkit: linear-gradient(${angle}deg, ${colorOne}, ${colorTwo}${colorThreeStyle});
         `
-
+    
     // Styling that will be passed to React component for presentation
     const activeStyle = {
         background: `linear-gradient(${angle}deg, ${colorOne}, ${colorTwo}${colorThreeStyle})`,
@@ -21,11 +24,23 @@ export default function PreviewPane(props) {
         MozBackground: `linear-gradient(${angle}deg, ${colorOne}, ${colorTwo}${colorThreeStyle})`
     }
 
+    // Set useRef of text
+    const outTextRef = useRef(null);
+
+    // create handler to copy text from code pane to user's Clipboard
+
+    const clickHandler = event => {
+        const content = outTextRef.current;
+        console.log(content.textContent)
+        navigator.clipboard.writeText(content.textContent)
+        alert("Copied!");
+    }
+
     return (
         <div className="preview-pane--main">
             <div style={activeStyle} className="preview-pane--display"></div>
-            <div className="preview-pane--code">
-                {cssPresentation.split("<br>").map((text, index) => <p key={index}>{text}</p>)}
+            <div ref={outTextRef} onClick={clickHandler} onDoubleClick={clickHandler} className="preview-pane--code">
+                {cssPresentation.split("<br>").map((text, index) => <React.Fragment key={index}>{text}{<br/>}</React.Fragment>)}
             </div>
         </div>
     )
