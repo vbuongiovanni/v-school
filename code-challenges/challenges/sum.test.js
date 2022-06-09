0b50efff13260edcb80a1978c2effd14c3f53a5d
Vince Buongiovanni <vincentbuongiovanni@gmail.com>
2022-06-08 22:51:25 -0600

    added intro work for jest, reorganized code-challenge directory, and 07-react directory within course-notes

---
+++ code-challenges/challenges/sum.test.js
@@ -0,0 +1,13 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 3 + 3 to equal 6', () => {
+  expect(sum(3, 3)).toBe(6);
+});
+
+test('adds 6 + -6 to equal 0', () => {
+  expect(sum(6, -6)).toBe(0);
+});
--- course-notes/07-react/react-router/vschool-tutorial/react-router-v6-useNavigate/client/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react' // Step 1
-import ReactDOM from 'react-dom' // Step 2
-import App from './App.js' // Step 3
-
-ReactDOM.render(<App/>,document.getElementById('root')) // Step 4
